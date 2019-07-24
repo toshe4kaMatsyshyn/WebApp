ca3c1c1eec497d6ca3209c7d4e71c555f4f93753
toshe4kaMatsyshyn <50492862+toshe4kaMatsyshyn@users.noreply.github.com>
2019-07-24 13:23:20 +0300

    Создание проекта

---
+++ DataLibrary/DataLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
