2509c1ef6aa306b0e2bc527bf210480a0e840940
Timothy <timaparf@gmail.com>
2019-07-26 12:14:02 +0300

    Test project to learn how api works
    Later i will change it to our project

---
+++ WebCore/WebCore/Models/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebCore.Models
+{
+    public class UserContext
+    {
+        public class UsersContext : DbContext
+        {
+            public DbSet<User> Users { get; set; }
+            public UsersContext(DbContextOptions<UsersContext> options)
+                : base(options)
+            { }
+        }
+    }
+}
--- WebCore/WebCore/Startup.cs
+++ WebCore/WebCore/Startup.cs
@@ -6,10 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using WebCore.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using static WebCore.Models.UserContext;
 
 namespace WebCore
 {
@@ -25,7 +28,9 @@ namespace WebCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            string con = "Server=(localdb)\\mssqllocaldb;Database=usersdbstore;Trusted_Connection=True;MultipleActiveResultSets=true";
+            services.AddDbContext<UsersContext>(options => options.UseSqlServer(con));
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
