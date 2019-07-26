f41d64306647cffef7a7549ab8a1c19af34404ca
timaparf <47056736+timaparf@users.noreply.github.com>
2019-07-26 13:14:02 +0300

    Web core for our project

---
+++ WebCore/WebCore/Models/Terminal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataLibrary
+{
+    public partial class Terminal
+    {
+        public Terminal()
+        {
+            TerminalsAndBrands = new HashSet<TerminalsAndBrands>();
+        }
+
+        public Terminal(string Name)
+        {
+            this.Name = Name;
+            Id = Settings.GenerateId();
+        }
+
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public int? ProducedBrands { get; set; } = 0;
+
+        public virtual ICollection<TerminalsAndBrands> TerminalsAndBrands { get; set; }
+    }
+}
