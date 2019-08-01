3365ef5c65e2b99914de2c1764b9a77865edaa8e
timaparf <47056736+timaparf@users.noreply.github.com>
2019-08-01 20:47:30 +0300

    Changes

--- WebCore/WebCore/Models/WebAppDatabaseContext.cs
+++ WebCore/WebCore/Models/WebAppDatabaseContext.cs
@@ -22,14 +22,7 @@ namespace WebCore.Models
         public virtual DbSet<Terminal> Terminal { get; set; }
         public virtual DbSet<TerminalsAndBrands> TerminalsAndBrands { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-GN3L1JT\\SQLEXPRESS;Database=WebAppDatabase;Trusted_Connection=True;");
-            }
-        }
+       
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
