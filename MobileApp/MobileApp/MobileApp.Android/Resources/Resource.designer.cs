99612a954fc423a048a1d5ed0e92b4b7c7ebf73e
toshe4kaMatsyshyn <50492862+toshe4kaMatsyshyn@users.noreply.github.com>
2019-08-01 20:05:37 +0300

    Test commit

--- MobileApp/MobileApp/MobileApp.Android/Resources/Resource.designer.cs
+++ MobileApp/MobileApp/MobileApp.Android/Resources/Resource.designer.cs
@@ -26,7 +26,6 @@ namespace MobileApp.Droid
 		
 		public static void UpdateIdValues()
 		{
-			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::MobileApp.Droid.Resource.Attribute.actionBarSize;
 		}
 		
 		public partial class Animation
