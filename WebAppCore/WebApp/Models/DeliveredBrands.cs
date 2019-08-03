1ce7872343a5dd4a468c688e17c1184511b480f3
timaparf <47056736+timaparf@users.noreply.github.com>
2019-08-03 13:18:25 +0300

    Frontend added

---
+++ WebAppCore/WebApp/Models/DeliveredBrands.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApp.Models
+{
+    public partial class DeliveredBrands
+    {
+        public DeliveredBrands()
+        {
+
+        }
+
+        public DeliveredBrands(ProducedBrands produced, int? CountOfDelivered)
+        {
+            this.ProduceBrands = produced;
+            ProduceBrandsId = produced.Id;
+
+            Id = Settings.GenerateId();
+            this.CountOfDelivered = CountOfDelivered;
+        }
+
+        public string ProduceBrandsId { get; set; }
+        public string Id { get; set; }
+        public int? CountOfDelivered { get; set; }
+
+        public virtual ProducedBrands ProduceBrands { get; set; }
+    }
+}
---
+++ WebAppCore/WebApp/Models/Terminal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApp.Models
+{
+    public partial class Terminal
+    {
+        public Terminal()
+        {
+            TerminalsAndBrands = new HashSet<TerminalsAndBrands>();
+        }
+
+        public Terminal(string Name, int? ProducedBrands)
+        {
+            this.Name = Name;
+            Id = Settings.GenerateId();
+            this.ProducedBrands = ProducedBrands;
+        }
+
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public int? ProducedBrands { get; set; }
+
+        public virtual ICollection<TerminalsAndBrands> TerminalsAndBrands { get; set; }
+    }
+}
